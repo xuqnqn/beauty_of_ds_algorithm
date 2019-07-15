d2c6f796c7e19970a4234ec7cf39937def3418c5
xuqnqn <xuqnqn@qq.com>
2019-07-16 07:38:41 +0800

    添加单链表

---
+++ Single_List.h
@@ -0,0 +1,111 @@
+/*************************************************************************
+      > File Name: Single_List.h
+      > Author: Xu Qingqing
+      > Mail: xuqnqn@qq.com
+      > Created Time: Tue 16 Jul 2019 07:36:51 AM CST
+ ************************************************************************/
+
+#ifndef _SINGLE_LIST_H
+#define _SINGLE_LIST_H
+
+#include <iostream>
+
+using namespace std;
+
+//带头节点的单链表，比不带头节点的实现简单
+template <typename T>
+class Single_List {
+    public:
+        Single_List();
+        ~Single_List();
+        Single_List(const Single_List & l) = delete;
+        Single_List & operator = (const Single_List & l) = delete;
+
+        struct Node {
+            Node(const T& a) : data(a), next(nullptr) {};
+            Node() : data(0), next(nullptr) {};
+            T data;
+            Node * next;
+        };
+
+        int insert(const T& a);
+        int remove(const T& a);
+        void dump();
+
+    private:
+        Node * head; 
+};
+
+template<typename T>
+Single_List<T> :: Single_List() {
+    try {
+        head = new Node;
+    } catch (exception &e) {
+        cout << "in Single list constructor: " << e.what() << endl;
+    }
+}
+
+template<typename T>
+Single_List<T> :: ~Single_List() {
+    Node * p;
+    while(head->next) {
+        p = head->next;
+        head->next = head->next->next;
+        delete p;
+    }
+    delete head;
+}
+
+template<typename T>
+int Single_List<T> :: insert(const T& a) {
+    cout << "inserting " << a << endl;
+    Node * p;
+    try {
+        p = new Node(a);    
+    } catch (exception &e) {
+        cout << "in Single list insert function:" << e.what() << endl;
+        return -1;
+    }
+    p->next = head->next;
+    head->next = p;
+    return 0;
+}
+
+template<typename T>
+int Single_List<T> :: remove(const T& a) {
+    cout << "removing " << a << endl;
+    Node *p = head->next;
+    Node *q = head;
+    while(p && p->data != a) {
+        q = p;
+        p = p->next;
+    }
+    if(p && p->data == a) {
+        q->next = p->next;
+        delete p;
+    } else {
+        cout << a << " is not found" << endl;
+        return -1;
+    }
+    return 0;
+}
+
+template<typename T>
+void Single_List<T> :: dump() {
+    if(!head->next){
+        cout << "List is empty" << endl;
+        return;
+    }
+
+    cout << "List element: ";
+    Node *p = head->next;
+    while(p) {
+        cout << p->data << " ";
+        p = p->next;
+    }
+    cout << endl;
+    return;
+}
+
+
+#endif
---
+++ day1_4_single_list.cc
@@ -0,0 +1,56 @@
+/*************************************************************************
+      > File Name: day1_4_single_list.cc
+      > Author: Xu Qingqing
+      > Mail: xuqnqn@qq.com
+      > Created Time: Tue 16 Jul 2019 06:39:17 AM CST
+ ************************************************************************/
+
+#include <iostream>
+#include "Single_List.h"
+
+using namespace std;
+
+int main(int argc, char **argv, char **env)
+{
+#if 0
+    Single_List<int> l;
+    l.dump();   //dump empty list
+
+    l.remove(7); //remove from empty list
+
+    l.insert(4); //insert a node
+    l.dump();
+
+    l.insert(7);
+    l.insert(1);
+    l.insert(0);
+    l.insert(3); //insert several nodes
+    l.dump();
+
+    l.remove(7); //remove a existed node
+    l.dump();
+
+    l.remove(79); //remove a non-existed node
+#else
+    Single_List<double> l;
+    l.dump();   //dump empty list
+
+    l.remove(7.3); //remove from empty list
+
+    l.insert(4.2); //insert a node
+    l.dump();
+
+    l.insert(7.2);
+    l.insert(1.0);
+    l.insert(0);
+    l.insert(3.5); //insert several nodes
+    l.dump();
+
+    l.remove(7.2); //remove a existed node
+    l.dump();
+
+    l.remove(79); //remove a non-existed node
+#endif
+
+    return 0;
+}
